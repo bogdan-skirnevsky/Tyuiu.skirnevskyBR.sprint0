18e91d59fa1f276f28bea98ab395439d213b4d85
Богдан Скирневский <tyuiu.skirnevskybr@outlook.com>
2025-09-09 18:25:03 +0300

    Task2

---
+++ Tyuiu.skirnevskyBR.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.skirnevskyBR.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.skirnevskyBR.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Богдан";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Богдан", res);
+        }
+    }
+}
---
+++ Tyuiu.skirnevskyBR.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿using Tyuiu.skirnevskyBR.Sprint0.Task2.V0.Lib;
+Console.WriteLine(DataService.GetMessage("Богдан"));
+Console.ReadKey(); 
+
+
+
